212c24f23978ef44b58c2d9154bffbcb3de75d7d
hoangnt1705 <chihienyeu@gmail.com>
2022-11-20 21:32:34 +0700

    video 6.1 generate model/migration/seeder

--- src/server.js
+++ src/server.js
@@ -1,10 +1,11 @@
 import express from "express";
-import configViewEngine from "./configs/viewEngine.js";
+import configViewEngine from "./config/viewEngine.js";
 import initWebRoutes from "./routes/web.js";
 if (process.env.NODE_ENV === 'development') {
   require('dotenv').config();
 }
 import bodyParser from "body-parser";
+// import connection from "./config/connectDB.js";
 const app = express();
 const PORT = process.env.PORT || 8080;
 //config view engine
@@ -12,6 +13,8 @@ configViewEngine(app);
 //config body-parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+//test connection 
+// connection();
 // init web router
 initWebRoutes(app);
 
